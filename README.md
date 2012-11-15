e99df1eb4f173ef69fd9bc3827c66e5a55b07620
Lakshan Perera <lakshan@web2media.net>
2012-11-15 06:56:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Punch Current Page Helper
+
+Simple helper that adds "current" class to a link if it points to the current page.
+
+This helper is intended to use with [Punch](http://laktek.github.com/punch) web publishing framework.
+
+## How to Use 
+
+* Install the package
+
+	`npm install punch-current-page-helper`
+
+* Open your Punch project's configurations (`config.json`) and add the following:
+
+		"plugins": {
+
+			"helpers": {
+				"current_page_helper": "./helpers/current_helper.js",
+			}
+
+		}
+
+* Then you can use the helper in your templates like this:
+
+		<a href="/about" class="{{#current}}/about{{/current}}">About</a>
+
+This will add the `current` class to the link when the user visits the `/about` page.
+
+**Note**: Make sure you always use relative URLs instead of absoulte URLs.
+
+## License
+
+Copyright (c) 2012 Lakshan Perera  
+Licensed under the MIT license.
---
+++ spec/current_page_helper.spec.js
@@ -0,0 +1,48 @@
+var current_page_helper_obj = require('../lib/current_page_helper.js');
+var current_page_helper = current_page_helper_obj.directAccess()["block_helpers"];
+
+describe("behaviour of current helper", function() {
+
+	it("add the current class for a section path", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/about/index", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("about")).toEqual("current");
+	});
+
+	it("add the current class for a sub section path", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/about/team/index", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("/about/team")).toEqual("current");
+	});
+
+	it("add the current class for a direct path", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/section/sub/path", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("/section/sub/path")).toEqual("current");
+	});
+
+	it("add the current class for a direct path with extension", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/section/sub/path", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("/section/sub/path.html")).toEqual("current");
+	});
+
+	it("don't add the current class if the paths doesn't match", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/section/sub/path", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("about")).toEqual("");
+	});
+
+	it("don't add the current class if no path given", function() {
+		var spyCallback = jasmine.createSpy();
+		current_page_helper_obj.get("/section/sub/path", ".html", {}, spyCallback);
+
+		expect(current_page_helper.current("")).toEqual("");
+	});
+
+});
